744c3bd36fbd039f5c973a512c211c4fd9859c67
edvardskruumins <edvards.kruumins@gmail.com>
2024-11-22 09:13:06 +0200

    Delete uzdevums5.py

--- uzdevums5.py
+++
@@ -1,8 +0,0 @@
-import re
-
-text = "@John: Šis ir lielisks produkts!!! Vai ne? 👏👏👏 http://example.com"
-
-cleaned_text = re.sub(r'[@#\*\.\(\\)]', '', text)
-cleaned_text = re.sub(r'http[^\s]+', '', cleaned_text) 
-
-print(cleaned_text)
